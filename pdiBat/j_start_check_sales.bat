398cdccb978d467db46a43b5ebcfaa68060fa90f
Victor Hugo Rosa <victorhugo.rosa@outlook.com>
2015-12-11 15:26:21 -0200

    adding bat to run auto

---
+++ pdiBat/j_start_check_sales.bat
@@ -0,0 +1,4 @@
+C:
+cd C:\Pentaho\data-integration
+
+kitchen.bat /file:C:\Pentaho\pentahoMaps\pdiAuditoria\j_start_check_sales.kjb /level:Normal > C:\Pentaho\pentahoLogs\pdiAuditoria\j_start_check_sales_%date:~-4,4%%date:~-10,2%%date:~-7,2%.txt
