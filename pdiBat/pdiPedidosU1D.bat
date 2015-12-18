15175469f57a04d1d50238581f76b210cccb3f68
victorhugosrosa <victorhugo.rosa@outlook.com>
2015-12-18 14:23:44 -0200

    creating pdiPedidosU1D .bat

---
+++ pdiBat/pdiPedidosU1D.bat
@@ -0,0 +1,4 @@
+C:
+cd C:\Pentaho\data-integration
+
+kitchen.bat /file:C:\Pentaho\pentahoMaps\pdiPEdidosU1D\j_start_pedidos_u1d.kjb /level:Normal > C:\Pentaho\pentahoLogs\pdiPEdidosU1D\pdiPEdidosU1D_%date:~-4,4%%date:~-10,2%%date:~-7,2%.txt
