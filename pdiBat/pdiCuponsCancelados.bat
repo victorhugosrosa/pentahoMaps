dc66825f033ff3e847a73a650cc86118f0788700
victorhugosrosa <victorhugo.rosa@outlook.com>
2016-05-23 11:39:05 -0300

    cancelados 33sem29

---
+++ pdiBat/pdiCuponsCancelados.bat
@@ -0,0 +1,4 @@
+C:
+cd C:\Pentaho\data-integration
+
+kitchen.bat /file:C:\Pentaho\pentahoMaps\pdiCuponsCancelados\j_start_CuponsCancelados.kjb /level:Normal > C:\Pentaho\pentahoLogs\pdiCuponsCancelados\CuponsCancelados_%date:~-4,4%%date:~-10,2%%date:~-7,2%.txt
