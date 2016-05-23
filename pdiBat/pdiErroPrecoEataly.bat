59f618bdb406c4a4d23f8cad622d0dc3143c4e94
victorhugosrosa <victorhugo.rosa@outlook.com>
2016-05-23 11:36:18 -0300

    bat precos

---
+++ pdiBat/pdiErroPrecoEataly.bat
@@ -0,0 +1,4 @@
+C:
+cd C:\Pentaho\data-integration
+
+kitchen.bat /file:C:\Pentaho\pentahoMaps\pdiErroPrecoEataly\j_start_ErroPrecoEataly.kjb /level:Normal > C:\Pentaho\pentahoLogs\pdiErroPrecoEataly\pdiErroPrecoEataly_%date:~-4,4%%date:~-10,2%%date:~-7,2%.txt
