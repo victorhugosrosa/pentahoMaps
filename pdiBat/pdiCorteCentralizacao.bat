28ac10bab79ca6958710e61dd0e8320a3b2ea1f2
victorhugosrosa <victorhugo.rosa@outlook.com>
2016-06-29 11:16:30 -0300

    bat pdiCorteCentralizacao

---
+++ pdiBat/pdiCorteCentralizacao.bat
@@ -0,0 +1,4 @@
+C:
+cd C:\Pentaho\data-integration
+
+kitchen.bat /file:C:\Pentaho\pentahoMaps\pdiCorteCentralizacao\j_start_CorteCentralizacao.kjb /level:Normal > C:\Pentaho\pentahoLogs\pdiCorteCentralizacao\pdiCorteCentralizacao_%date:~-4,4%%date:~-10,2%%date:~-7,2%.txt
