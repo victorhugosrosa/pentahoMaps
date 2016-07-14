d6deefd3894832b5e8f5faf3d2445a162f77b4e5
victorhugosrosa <victorhugo.rosa@outlook.com>
2016-07-14 20:21:22 -0300

    pdiAuditoriaCentralizacao bat

---
+++ pdiBat/pdiAuditoriaCentralizacao.bat
@@ -0,0 +1,4 @@
+C:
+cd C:\Pentaho\data-integration
+
+kitchen.bat /file:C:\Pentaho\pentahoMaps\pdiAuditoriaCentralizacao\j_start_AuditoriaCentralizacao.kjb /level:Normal > C:\Pentaho\pentahoLogs\pdiAuditoriaCentralizacao\pdiAuditoriaCentralizacao_%date:~-4,4%%date:~-10,2%%date:~-7,2%.txt
