2d91f5ef2299d247ed797ce3128ef9a4bffe4695
victorhugosrosa <victorhugo.rosa@outlook.com>
2016-08-17 10:48:30 -0300

    pdiAuditoriaCustoIP

---
+++ pdiBat/pdiAuditoriaCustoIP.bat
@@ -0,0 +1,4 @@
+C:
+cd C:\Pentaho\data-integration
+
+kitchen.bat /file:C:\Pentaho\pentahoMaps\pdiAuditoriaCustoIP\j_start_AuditoriaCustoIP.kjb /level:Normal > C:\Pentaho\pentahoLogs\pdiAuditoriaCustoIP\pdiAuditoriaCustoIP_%date:~-4,4%%date:~-10,2%%date:~-7,2%.txt
