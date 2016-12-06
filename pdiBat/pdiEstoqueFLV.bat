53026807b67999a8231fc5cdb67c3862bec769c6
victorhugosrosa <victorhugo.rosa@outlook.com>
2016-12-06 17:41:45 -0200

    bat estoque flv

---
+++ pdiBat/pdiEstoqueFLV.bat
@@ -0,0 +1,4 @@
+C:
+cd C:\Pentaho\data-integration
+
+kitchen.bat /file:C:\Pentaho\pentahoMaps\pdiEstoqueFLV\j_start_pdiEstoqueFLV.kjb /level:Normal > C:\Pentaho\pentahoLogs\pdiEstoqueFLV\pdiEstoqueFLV_%date:~-4,4%%date:~-10,2%%date:~-7,2%.txt
