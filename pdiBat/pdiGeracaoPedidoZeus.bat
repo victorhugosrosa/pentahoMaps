50d4e8122a8c3f0a6c4e333697fda35f18eed13b
victorhugosrosa <victorhugo.rosa@outlook.com>
2016-12-16 17:24:16 -0200

    pdiGeracaoPedidoZeus

---
+++ pdiBat/pdiGeracaoPedidoZeus.bat
@@ -0,0 +1,4 @@
+C:
+cd C:\Pentaho\data-integration
+
+kitchen.bat /file:C:\Pentaho\pentahoMaps\pdiGeracaoPedidoZeus\j_start_pdiGeracaoPedidoZeus.kjb /level:Normal > C:\Pentaho\pentahoLogs\pdiGeracaoPedidoZeus\j_start_pdiGeracaoPedidoZeus_%date:~-4,4%%date:~-10,2%%date:~-7,2%.txt
