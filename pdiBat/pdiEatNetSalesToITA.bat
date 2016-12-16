895f6ef0a25a32db6240d4217ed20e0053bc7a64
victorhugosrosa <victorhugo.rosa@outlook.com>
2016-12-16 17:24:25 -0200

    bat pdiEatNetSalesToITA

---
+++ pdiBat/pdiEatNetSalesToITA.bat
@@ -0,0 +1,4 @@
+C:
+cd C:\Pentaho\data-integration
+
+kitchen.bat /file:C:\Pentaho\pentahoMaps\pdiEatNetSalesToITA\pdiEatNetSalesToITA.kjb /level:Normal > C:\Pentaho\pentahoLogs\pdiEatNetSalesToITA\pdiEatNetSalesToITA_%date:~-4,4%%date:~-10,2%%date:~-7,2%.txt
